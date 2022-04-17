d799669759dc9e232402eff7c1b0b310a18addea
sahilrawat001 <sahilrawat80@gmail.com>
2022-04-18 00:30:56 +0530

    added code

--- src/PortfolioC/Project/Project.jsx
+++ src/PortfolioC/Project/Project.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import "./Project.css";
-const Project=()=> {
-    return (
-       <>
-       <div id="projects"></div>
-       <div   className="container_project">
-           <h1 className="head">
-               MY PROJECTS
-           </h1>
-       </div>
-       </>
-    )
-}
+import React from "react";
+import Slider from "react-slick";
+import "./Project.css"
+ import LeftArrow from "../assets/left-arrow.svg"
+import RightArrow from "../assets/right-arrow.svg"
+ 
+ 
 
-export default Project
+const Project=({title,datas})=> {
+
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    <img src={LeftArrow} alt="prevArrow" {...props} />
+  );
+
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <img src={RightArrow} alt="nextArrow" {...props} />
+  );
+    const settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      prevArrow: <SlickArrowLeft />,
+    nextArrow: <SlickArrowRight />,
+    };
+    return (
+     <div className="card__container">
+   <h1>{title}</h1>
+ <Slider {...settings} className="card__container--inner">
+          {datas.map((item, index) => {
+            return (
+              <div
+              className="card__container--inner--card"
+              key={index}>
+                
+
+                <img src={item.url} alt="hero_img" />
+              
+               
+
+
+               
+              </div>
+            );
+          })}
+        </Slider>
+     </div>
+       
+    );
+  }
+  export default Project;
+ 
--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ import Footer from './PortfolioC/Footer/Footer';
  import Intro from './PortfolioC/Intro/Intro';
 import Navbar from './PortfolioC/Navbar/Navbar';
 import Project from './PortfolioC/Project/Project';
-  
+ 
+const dataForBackPacking = [
+  {url:"https://www.wanderon.in/triplist/bir-billing/wanderon-bir-1.jpg"},
+  {url:"https://www.wanderon.in/triplist/manali-lahaul/wanderon-manali-1.jpg"},
+  {url:"https://www.wanderon.in/triplist/kasol-kheerganga/wanderon-kasol-1.jpg"},
+  {url:"https://www.wanderon.in/triplist/tirthan-valley/wanderon-tirthan-1.jpg"},
+  {url:"https://www.wanderon.in/triplist/chopta-tungnath/wanderon-chopta-1.jpg"},
+  {url:"https://www.wanderon.in/triplist/mcleodganj-bir-billing/wanderon-bir-1.jpg"},
+]
+
+
 ReactDOM.render(
   <React.StrictMode>
      <Navbar/>
      <Links/>
      <Intro/>
      <About/>
-     <Project/>
+     <Project title="Explore WanderOn" datas = {dataForBackPacking} />
+
      <Contact/>
      <Footer/>
   </React.StrictMode>,
